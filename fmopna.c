c00355eb3b8d540bcbda8be5c3b5f6b26771db83
nukeykt <alexeytf2@gmail.com>
2024-01-04 17:49:46 +0900

    wip

--- fmopna.c
+++ fmopna.c
@@ -196,6 +196,7 @@ void FMOPNA_Clock(fmopna_t *chip, int clk)
 
         chip->ssg_write0 = writeaddr && !chip->input.a1;
         chip->ssg_write1 = (writedata && !chip->input.a1) || chip->ic;
+        chip->ssg_read1 = read1;
 
         if (writedata)
             chip->write0_trig0 = 1;
@@ -282,34 +283,10 @@ void FMOPNA_Clock(fmopna_t *chip, int clk)
             chip->data_bus1 &= ~255;
             chip->data_bus1 |= 1;
         }
-        else if (read1 && !chip->tm_w1)
-        {
-            chip->data_bus1 &= ~255;
-            chip->data_bus1 |= chip->input.gpio_a & 255;
-        }
-        else if (read1 && !chip->tm_w1)
-        {
-            chip->data_bus1 &= ~255;
-            chip->data_bus1 |= chip->input.gpio_a & 255;
-        }
-        // TODO: more data bus
 
         if (read1 || read3)
             chip->read_bus = chip->data_bus1;
         // TODO: more read bus
-
-
-        if (chip->ssg_write1 && !chip->tm_w2)
-            chip->o_gpio_a = chip->data_bus1 & 255;
-        //else if (!chip->ssg_write1)
-        //    chip->o_gpio_a = chip->o_gpio_a;
-        chip->o_gpio_a_d = chip->tm_w1;
-
-        if (chip->ssg_write1 && !chip->tm_w2)
-            chip->o_gpio_b = chip->data_bus1 & 255;
-        //else if (!chip->ssg_write1)
-        //    chip->o_gpio_b = chip->o_gpio_b;
-        chip->o_gpio_b_d = chip->tm_w1;
 
 #define ADDRESS_MATCH(x) ((chip->data_bus2 & x) == 0 && (chip->data_bus1 & (x^511)) == 0)
         if (chip->mclk1)
@@ -1817,6 +1794,319 @@ void FMOPNA_Clock(fmopna_t *chip, int clk)
 
         chip->ssg_clk1 = !chip->ssg_clk;
         chip->ssg_clk2 = chip->ssg_clk;
+
+        if (chip->ic)
+            chip->ssg_ssg_addr = 0;
+        else if (chip->ssg_write0)
+            chip->ssg_ssg_addr = (chip->data_bus1 & 0xf0) == 0;
+
+        if (chip->ic)
+            chip->ssg_address = 0;
+        else if (chip->ssg_write0 && (chip->data_bus1 & 0xe0) == 0)
+            chip->ssg_address = chip->data_bus1 & 0x1f;
+        
+        int ssg_access = chip->ssg_ssg_addr && (chip->ssg_write1 || chip->ssg_read1);
+
+        if (chip->ssg_egtrig_rst)
+            chip->ssg_egtrig = 0;
+
+        if (chip->ic)
+        {
+            chip->ssg_freq_a[0] = chip->data_bus1 & 255;
+            chip->ssg_freq_a[1] = chip->data_bus1 & 15;
+            chip->ssg_freq_b[0] = chip->data_bus1 & 255;
+            chip->ssg_freq_b[1] = chip->data_bus1 & 15;
+            chip->ssg_freq_c[0] = chip->data_bus1 & 255;
+            chip->ssg_freq_c[1] = chip->data_bus1 & 15;
+            chip->ssg_noise = chip->data_bus1 & 31;
+            chip->ssg_mode = chip->data_bus1 & 255;
+            chip->ssg_level_a = chip->data_bus1 & 31;
+            chip->ssg_level_b = chip->data_bus1 & 31;
+            chip->ssg_level_c = chip->data_bus1 & 31;
+            chip->ssg_env[0] = chip->data_bus1 & 255;
+            chip->ssg_env[1] = chip->data_bus1 & 255;
+            chip->ssg_envmode = chip->data_bus1 & 15;
+            chip->o_gpio_a = chip->data_bus1 & 255;
+            chip->o_gpio_b = chip->data_bus1 & 255;
+
+            chip->ssg_egtrig = 1;
+        }
+        else if (chip->ssg_ssg_addr && chip->ssg_write1)
+        {
+            switch (chip->ssg_address)
+            {
+                case 0x0:
+                    chip->ssg_freq_a[0] = chip->data_bus1 & 255;
+                    break;
+                case 0x1:
+                    chip->ssg_freq_a[1] = chip->data_bus1 & 15;
+                    break;
+                case 0x2:
+                    chip->ssg_freq_b[0] = chip->data_bus1 & 255;
+                    break;
+                case 0x3:
+                    chip->ssg_freq_b[1] = chip->data_bus1 & 15;
+                    break;
+                case 0x4:
+                    chip->ssg_freq_c[0] = chip->data_bus1 & 255;
+                    break;
+                case 0x5:
+                    chip->ssg_freq_c[1] = chip->data_bus1 & 15;
+                    break;
+                case 0x6:
+                    chip->ssg_noise = chip->data_bus1 & 31;
+                    break;
+                case 0x7:
+                    chip->ssg_mode = chip->data_bus1 & 255;
+                    break;
+                case 0x8:
+                    chip->ssg_level_a = chip->data_bus1 & 31;
+                    break;
+                case 0x9:
+                    chip->ssg_level_b = chip->data_bus1 & 31;
+                    break;
+                case 0xa:
+                    chip->ssg_level_c = chip->data_bus1 & 31;
+                    break;
+                case 0xb:
+                    chip->ssg_env[0] = chip->data_bus1 & 255;
+                    break;
+                case 0xc:
+                    chip->ssg_env[1] = chip->data_bus1 & 255;
+                    break;
+                case 0xd:
+                    chip->ssg_envmode = chip->data_bus1 & 15;
+                    chip->ssg_egtrig = 1;
+                    break;
+                case 0xe:
+                    chip->o_gpio_a = chip->data_bus1 & 255;
+                    break;
+                case 0xf:
+                    chip->o_gpio_b = chip->data_bus1 & 255;
+                    break;
+            }
+        }
+        if (chip->ssg_ssg_addr && chip->ssg_read1)
+        {
+            switch (chip->ssg_address)
+            {
+                case 0x0:
+                    chip->data_bus1 &= ~255;
+                    chip->data_bus1 |= chip->ssg_freq_a[0] & 255;
+                    break;
+                case 0x1:
+                    chip->data_bus1 &= ~15;
+                    chip->data_bus1 |= chip->ssg_freq_a[1] & 15;
+                    break;
+                case 0x2:
+                    chip->data_bus1 &= ~255;
+                    chip->data_bus1 |= chip->ssg_freq_b[0] & 255;
+                    break;
+                case 0x3:
+                    chip->data_bus1 &= ~15;
+                    chip->data_bus1 |= chip->ssg_freq_b[1] & 15;
+                    break;
+                case 0x4:
+                    chip->data_bus1 &= ~255;
+                    chip->data_bus1 |= chip->ssg_freq_c[0] & 255;
+                    break;
+                case 0x5:
+                    chip->data_bus1 &= ~15;
+                    chip->data_bus1 |= chip->ssg_freq_c[1] & 15;
+                    break;
+                case 0x6:
+                    chip->data_bus1 &= ~31;
+                    chip->data_bus1 |= chip->ssg_noise & 31;
+                    break;
+                case 0x7:
+                    chip->data_bus1 &= ~255;
+                    chip->data_bus1 |= chip->ssg_mode & 255;
+                    break;
+                case 0x8:
+                    chip->data_bus1 &= ~31;
+                    chip->data_bus1 |= chip->ssg_level_a & 31;
+                    break;
+                case 0x9:
+                    chip->data_bus1 &= ~31;
+                    chip->data_bus1 |= chip->ssg_level_b & 31;
+                    break;
+                case 0xa:
+                    chip->data_bus1 &= ~31;
+                    chip->data_bus1 |= chip->ssg_level_c & 31;
+                    break;
+                case 0xb:
+                    chip->data_bus1 &= ~255;
+                    chip->data_bus1 |= chip->ssg_env[0] & 255;
+                    break;
+                case 0xc:
+                    chip->data_bus1 &= ~255;
+                    chip->data_bus1 |= chip->ssg_env[1] & 255;
+                    break;
+                case 0xd:
+                    chip->data_bus1 &= ~15;
+                    chip->data_bus1 |= chip->ssg_envmode & 15;
+                    break;
+                case 0xe:
+                    chip->data_bus1 &= ~15;
+                    chip->data_bus1 |= chip->input.gpio_a & 15;
+                    break;
+                case 0xf:
+                    chip->data_bus1 &= ~255;
+                    chip->data_bus1 |= chip->input.gpio_b & 255;
+                    break;
+            }
+        }
+
+        if (chip->ssg_clk1)
+        {
+            chip->ssg_envadd = (chip->ssg_eg_of[1] & 4) != 0 && (chip->ssg_sel[1] & 8) != 0;
+            chip->ssg_envcnt[1] = chip->ssg_hold[0] ? 31 : chip->ssg_envcnt[0];
+            chip->ssg_dir[1] = chip->ssg_dir[0];
+            chip->ssg_hold[1] = chip->ssg_hold[0];
+            chip->ssg_t2[1] = chip->ssg_t2[0];
+
+            chip->ssg_egtrig_s = chip->ssg_egtrig;
+
+            chip->ssg_eg_sel_l = (chip->ssg_sel[1] & 8) != 0;
+
+            chip->ssg_sel[0] = chip->ssg_sel[1] << 1;
+            chip->ssg_sel[0] |= (chip->ssg_sel[1] & 7) == 0 && !chip->ic;
+
+            chip->ssg_sel_freq = 0;
+            if (chip->ssg_sel[1] & 1)
+                chip->ssg_sel_freq |= (chip->ssg_freq_c[1] << 8) | chip->ssg_freq_c[0];
+            if (chip->ssg_sel[1] & 2)
+                chip->ssg_sel_freq |= (chip->ssg_freq_b[1] << 8) | chip->ssg_freq_b[0];
+            if (chip->ssg_sel[1] & 4)
+                chip->ssg_sel_freq |= (chip->ssg_freq_a[1] << 8) | chip->ssg_freq_a[0];
+            if (chip->ssg_sel[1] & 8)
+                chip->ssg_sel_freq |= (chip->ssg_env[1] << 8) | chip->ssg_env[0];
+
+            int cnt = chip->ssg_freq_cnt[7] + 1;
+            int of = (cnt & 0x1000) != 0;
+            chip->ssg_freq_cnt[0] = cnt & 0xfff;
+            chip->ssg_freq_cnt[4] = chip->ssg_freq_cnt[3];
+            chip->ssg_freq_cnt[6] = chip->ssg_freq_cnt[5];
+
+            chip->ssg_cnt_of[1] = chip->ssg_cnt_of[0];
+
+            if (chip->ssg_sel[1] & 8)
+                chip->ssg_sign_toggle = chip->ssg_cnt_of[0] & 7;
+            else
+                chip->ssg_sign_toggle = 0;
+
+            chip->ssg_freq_cnt2[1] = chip->ssg_freq_cnt2[0];
+            chip->ssg_freq_cnt2[3] = chip->ssg_freq_cnt2[2];
+            chip->ssg_freq_cnt2[5] = chip->ssg_freq_cnt2[4];
+            chip->ssg_freq_cnt2[7] = chip->ssg_freq_cnt2[6];
+
+            int cnt_of = !chip->ssg_cnt_reload && ((chip->ssg_sel_freq_l + chip->ssg_freq_cnt[0] + chip->ssg_cnt_of_l) & 0x10) != 0;
+
+            chip->ssg_cnt2_add = (chip->ssg_sel[1] & 8) != 0 && of;
+
+            chip->ssg_sel_eg_l[0] = (chip->ssg_sel[1] & 8) != 0;
+
+            chip->ssg_eg_of[0] = (chip->ssg_eg_of[1] << 1) | cnt_of;
+
+            int fr_rst = chip->ssg_ch_of || ((cnt_of || chip->ssg_cnt_reload) && chip->ssg_sel_eg_l[1]);
+
+            chip->ssg_freq_cnt[2] = fr_rst ? 0 : chip->ssg_freq_cnt[1];
+
+            chip->ssg_fr_rst_l = fr_rst;
+
+            chip->ssg_noise_add = (chip->ssg_sel[1] & 1) != 0;
+
+            chip->ssg_noise_cnt[1] = chip->ssg_noise_cnt[0];
+
+            int noise_of = (((chip->ssg_noise_cnt[0] >> 1) + chip->ssg_noise) & 32) != 0;
+
+            chip->ssg_noise_of = noise_of && chip->ssg_noise_of_low;
+
+            chip->ssg_test = chip->input.test;
+
+            chip->ssg_noise_step = chip->ssg_noise_of || chip->ssg_test;
+        }
+        if (chip->ssg_clk2)
+        {
+            int sum = chip->ssg_envcnt[1] + chip->ssg_envadd;
+            int of = (sum & 32) != 0;
+            chip->ssg_envcnt[0] = chip->ssg_egtrig_s ? 0 : sum & 31;
+
+            int dir = of && (chip->ssg_envmode & 2) != 0 && !chip->ssg_hold[1];
+            chip->ssg_dir[0] = chip->ssg_egtrig_s ? 0 : chip->ssg_dir[1] ^ dir;
+
+            chip->ssg_hold[0] = chip->ssg_egtrig_s ? 0 : chip->ssg_hold[1] || (of && (chip->ssg_envmode & 1) != 0);
+
+            chip->ssg_t2[0] = chip->ssg_egtrig_s ? 0 : chip->ssg_t2[1] || of;
+
+            chip->ssg_egtrig_rst = chip->ssg_egtrig_s && chip->ssg_eg_sel_l;
+
+
+            chip->ssg_sel[1] = chip->ssg_sel[0];
+
+            chip->ssg_freq_cnt[1] = chip->ssg_freq_cnt[0];
+            chip->ssg_freq_cnt[3] = chip->ssg_freq_cnt[2];
+            chip->ssg_freq_cnt[5] = chip->ssg_freq_cnt[4];
+            chip->ssg_freq_cnt[7] = chip->ssg_freq_cnt[6];
+
+            int cnt_of = (((chip->ssg_sel_freq & 0xfff) + chip->ssg_freq_cnt[0] + 1) & 0x1000) != 0;
+
+            chip->ssg_cnt_of_l = cnt_of;
+            chip->ssg_sel_freq_l = (chip->ssg_sel_freq >> 12) & 0xf;
+
+            chip->ssg_cnt_of[0] = (chip->ssg_cnt_of[1] << 1) | cnt_of;
+
+            if (chip->ic)
+                chip->ssg_sign[0] = 0;
+            else
+                chip->ssg_sign[0] = chip->ssg_sign[1] ^ chip->ssg_sign_toggle;
+
+            int cnt = chip->ssg_freq_cnt2[7] + chip->ssg_cnt2_add;
+            chip->ssg_freq_cnt2[0] = cnt & 0xf;
+            chip->ssg_freq_cnt2[2] = chip->ssg_fr_rst_l ? 0 : chip->ssg_freq_cnt2[1];
+            chip->ssg_freq_cnt2[4] = chip->ssg_freq_cnt2[3];
+            chip->ssg_freq_cnt2[6] = chip->ssg_freq_cnt2[5];
+
+            chip->ssg_sel_eg_l[1] = chip->ssg_sel_eg_l[0];
+
+            chip->ssg_ch_of = (!chip->ssg_sel_eg_l[0] && cnt_of) || chip->ic;
+
+            chip->ssg_cnt_reload = chip->ssg_sel_eg_l[0] && chip->ssg_egtrig_s;
+
+            chip->ssg_eg_of[1] = chip->ssg_eg_of[0];
+
+
+            chip->ssg_noise_cnt[0] = noise_step ? 0 : ((chip->ssg_noise_cnt[1] + chip->ssg_noise_add) & 63);
+
+            chip->ssg_noise_of_low = (chip->ssg_noise_cnt1[1] & 1) != 0 && chip->ssg_noise_add;
+
+        }
+
+        if (!chip->ssg_noise_step)
+        {
+            int bit = ((chip->ssg_noise_lfsr[1] >> 16) ^ (chip->ssg_noise_lfsr[1] >> 13)) & 1;
+
+            if ((chip->ssg_noise_lfsr[1] & 0x1ffff) == 0)
+                bit |= 1;
+
+            chip->ssg_noise_lfsr[0] = (chip->ssg_noise_lfsr[1] << 1) | bit;
+        }
+        else
+        {
+            chip->ssg_noise_lfsr[1] = chip->ic ? 0 : chip->ssg_noise_lfsr[0];
+        }
+
+        if (chip->ssg_clk2)
+            chip->ssg_noise_bit = (chip->ssg_noise_lfsr[1] >> 16) & 1;
+
+        int envlevel = chip->ssg_hold[0] ? 31 : chip->ssg_envcnt[0];
+        envlevel = (chip->ssg_dir[0] ^ ((chip->ssg_envmode >> 2) & 1)) ? (envlevel ^ 31) : envlevel;
+        envlevel = (chip->ssg_t2[0] && (chip->ssg_envmode & 8) == 0) ? 0 : envlevel;
+
+        int sign_a = ((chip->ssg_mode & 1) == 0 && (chip->ssg_sign[0] & 1) != 0) || ((chip->ssg_mode & 8) == 0 && chip->ssg_noise_bit);
+        int sign_b = ((chip->ssg_mode & 2) == 0 && (chip->ssg_sign[0] & 2) != 0) || ((chip->ssg_mode & 16) == 0 && chip->ssg_noise_bit);
+        int sign_c = ((chip->ssg_mode & 4) == 0 && (chip->ssg_sign[0] & 4) != 0) || ((chip->ssg_mode & 32) == 0 && chip->ssg_noise_bit);
+
     }
 #undef ADDRESS_MATCH
 }
--- fmopna.h
+++ fmopna.h
@@ -10,7 +10,8 @@ typedef struct {
     int a1;
     int data;
     int gpio_a;
-    int test;
+    int gpio_b;
+    int test; // set to 1
 } fmopna_input_t;
 
 typedef struct {
@@ -46,6 +47,7 @@ typedef struct {
 
     int ssg_write0;
     int ssg_write1;
+    int ssg_read1;
 
     int write0_trig0;
     int write0_trig1;
@@ -370,6 +372,50 @@ typedef struct {
     int ssg_clk;
     int ssg_clk1;
     int ssg_clk2;
+    int ssg_ssg_addr;
+    int ssg_address;
+    int ssg_freq_a[2];
+    int ssg_freq_b[2];
+    int ssg_freq_c[2];
+    int ssg_noise;
+    int ssg_mode;
+    int ssg_level_a;
+    int ssg_level_b;
+    int ssg_level_c;
+    int ssg_env[2];
+    int ssg_envmode;
+    int ssg_envadd;
+    int ssg_envcnt[2];
+    int ssg_dir[2];
+    int ssg_hold[2];
+    int ssg_t2[2];
+    int ssg_egtrig;
+    int ssg_egtrig_s;
+    int ssg_egtrig_rst;
+    int ssg_eg_sel_l;
+    int ssg_sel[2];
+    int ssg_sel_freq;
+    int ssg_freq_cnt[8];
+    int ssg_cnt_of[2];
+    int ssg_sign[2];
+    int ssg_sign_toggle;
+    int ssg_cnt_of_l;
+    int ssg_freq_cnt2[8];
+    int ssg_sel_freq_l;
+    int ssg_cnt2_add;
+    int ssg_sel_eg_l[2];
+    int ssg_ch_of;
+    int ssg_cnt_reload;
+    int ssg_eg_of[2];
+    int ssg_fr_rst_l;
+    int ssg_noise_add;
+    int ssg_noise_cnt[2];
+    int ssg_noise_of_low;
+    int ssg_noise_of;
+    int ssg_test;
+    int ssg_noise_step;
+    int ssg_noise_lfsr[2];
+    int ssg_noise_bit;
 
 
     int tm_w1;
